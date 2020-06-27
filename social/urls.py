f046fd1ea6e3e077e5382e27e084db7746fe6c20
ecoltri2 <eduardo.coltri@dxc.com>
2020-06-27 17:07:20 -0300

    PostDeleteView added

--- social/urls.py
+++ social/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('topic/<int:id>/posts/',views.PostListView.as_view(),name='post_list'),
     path('topic/<int:id>/posts/<int:post_id>/',views.PostDetailView.as_view(),name='post_detail'),
     path('create_post/',views.PostCreateView.as_view(),name='post_create'),
+    path('delete_post/<int:post_id>',views.PostDeleteView.as_view(),name='post_delete'),
 ]
--- social/views.py
+++ social/views.py
@@ -6,7 +6,7 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
 from . import models
 from django.contrib.auth.models import Group,User
-from django.urls import reverse
+from django.urls import reverse,reverse_lazy
 from django.http import HttpResponseRedirect
 
 class HomeView(TemplateView):
@@ -16,7 +16,6 @@ class HomeView(TemplateView):
         context = super().get_context_data(**kwargs)
         context["title"] = "Home"
         return context
-
 
 
 class PostDetailView(LoginRequiredMixin,PermissionRequiredMixin,DetailView):
@@ -38,12 +37,28 @@ class PostCreateView(LoginRequiredMixin,PermissionRequiredMixin,CreateView):
     permission_required = ('social.create_post')
     fields = ('title','topic','content')
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Create Post"
+        return context
+    
     def form_valid(self, form):
         user = get_object_or_404(User,pk = self.request.user.pk)
         post = form.save(commit=False)
         post.author = user
         post.save()
         return HttpResponseRedirect(reverse('social:post_detail',kwargs={'id':self.kwargs['id'],'post_id':post.id}))
+
+
+class PostDeleteView(LoginRequiredMixin,PermissionRequiredMixin,DeleteView):
+    model = models.Post
+    template_name = "social/post_delete.html"
+    permission_required=('social.delete_post')
+    slug_field = 'id'
+    slug_url_kwarg = 'post_id'
+
+    def get_success_url(self):
+        return reverse_lazy('accounts:profile',kwargs={'id':self.request.user.pk})
 
 class PostListView(LoginRequiredMixin,PermissionRequiredMixin,ListView):
     model = models.Post
@@ -56,7 +71,10 @@ class PostListView(LoginRequiredMixin,PermissionRequiredMixin,ListView):
         context['topic_id'] = self.kwargs['id']
         return context
     
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
 
     def get_queryset(self):
         topic = get_object_or_404(Group,pk=self.kwargs['id'])
-        return get_list_or_404(models.Post,topic=topic,is_published=True)
+        return models.Post.objects.filter(topic=topic,is_published=True)
