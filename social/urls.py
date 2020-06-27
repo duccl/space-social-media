0d4e214ceaf94cce859ec8361496a2b02b13e769
ecoltri2 <eduardo.coltri@dxc.com>
2020-06-27 15:33:45 -0300

    PostDetailView added

--- social/urls.py
+++ social/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .import views
 urlpatterns = [
     path('',views.HomeView.as_view(),name='home'),
-    path('topic/<int:id>/posts/',views.PostListView.as_view(),name='post_list')
+    path('topic/<int:id>/posts/',views.PostListView.as_view(),name='post_list'),
+    path('posts/<int:id>/',views.PostDetailView.as_view(),name='post_detail'),
 ]
--- social/views.py
+++ social/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from django.shortcuts import get_object_or_404,get_list_or_404
-from django.views.generic import ListView
+from django.views.generic import ListView,CreateView,DeleteView
 from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
 from . import models
@@ -16,14 +16,23 @@ class HomeView(TemplateView):
         return context
     
 class PostDetailView(LoginRequiredMixin,PermissionRequiredMixin,DetailView):
-    permission_required = ('social.view_bar')
+    permission_required = ('social.view_post')
     model = models.Post
     template_name = "social/post_detail.html"
+    slug_field = 'id'
+    slug_url_kwarg = 'id'
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = self.get_object().title
+        return context
+    
 
 class PostListView(LoginRequiredMixin,PermissionRequiredMixin,ListView):
     model = models.Post
     template_name = "social/post_list.html"
-    permission_required = ('social.view_bar')
+    permission_required = ('social.view_post')
     context_object_name = 'posts'
 
     def get_queryset(self):
