ffa4676babac3ef460cf0e1398d862a0782e5b4e
ecoltri2 <eduardo.coltri@dxc.com>
2020-06-26 21:19:22 -0300

    Change in ProfileView on function get

--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.views.generic import RedirectView
 from django.contrib.auth import logout
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect,HttpResponseForbidden
 from django.views.generic.detail import DetailView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
 from django.views.generic.edit import CreateView
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
@@ -49,6 +49,13 @@ class ProfileView(LoginRequiredMixin,DetailView):
     slug_url_kwarg = 'id'
     template_name = 'accounts/profile.html'
     login_url = 'accounts:login'
+    current_user = None
+
+    def get(self, request, *args, **kwargs):
+        if self.get_object().pk != request.user.pk:
+          return HttpResponseForbidden(content='Hey! This is not your profile!')
+        return super().get(request, *args, **kwargs)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["title"] = context['user'].username
