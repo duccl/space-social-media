e7ce043aeb4bd847e20f2d7cd24eeb7cc77272f6
ecoltri2 <eduardo.coltri@dxc.com>
2020-07-11 15:20:24 -0300

    Self Adjusting to screen size at profile.html

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import Group
 from django.urls import reverse
+from django import template
+register = template.Library()
 class Topic(Group):
     description = models.CharField(max_length=50)
     creation_date = models.DateTimeField(auto_now=True)
