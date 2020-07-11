4b52eea1aec812579155a2344ea6908ff432a7ea
ecoltri2 <eduardo.coltri@dxc.com>
2020-07-11 15:23:36 -0300

    Site added to allowed hosts

--- spaceSocialMedia/settings.py
+++ spaceSocialMedia/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = secretKey()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://duccl.pythonanywhere.com']
 
 
 # Application definition
